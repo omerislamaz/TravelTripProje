54ec5e99c9a37242702fcbd2efd82eb900e7e1d5
omerislamaz <omerislamaz@gmail.com>
2023-10-03 04:40:21 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Sınıflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+      
+            var degerler = c.Blogs.OrderByDescending(x=>x.ID).ToList();
+            return View(degerler);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public PartialViewResult Partial3()
+        {
+            var deger = c.Blogs.Take(10).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial4()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Partial5()
+        {
+            var deger = c.Blogs.Take(3).OrderByDescending(x=>x.ID).ToList();
+            return PartialView(deger);
+        }
+    }
+}
